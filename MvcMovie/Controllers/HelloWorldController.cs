b5179a69ed2b07c512490ef84309f91b512e14eb
Gaurav Garg <gaugar@microsoft.com>
2019-07-30 17:39:33 +0530

    First Commit

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name,int numTimes=1) {
+            return HtmlEncoder.Default.Encode($"{name} :{numTimes}");
+        }
+    }
+}
